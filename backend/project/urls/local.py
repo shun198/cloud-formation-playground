d7f87ad2e52bf2fc6e5919b3c2ec3a06c61d600b
s.hirose <shunichi_hirose@pj100.biz>
2024-01-26 15:02:08 +0900

    👍 add debug toolbar to dependencies

--- backend/project/urls/local.py
+++ backend/project/urls/local.py
@@ -7,6 +7,7 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+
 from project.settings.base import MEDIA_ROOT, MEDIA_URL
 from project.urls.base import urlpatterns
 
