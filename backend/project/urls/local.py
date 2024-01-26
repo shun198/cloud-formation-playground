90d7f9a971342bb7fbe4e3ca87aad91e36f1ece4
s.hirose <shunichi_hirose@pj100.biz>
2024-01-26 15:12:48 +0900

    👍 delete debugtoolbar

--- backend/project/urls/local.py
+++ backend/project/urls/local.py
@@ -1,18 +1,16 @@
 """Dev環境用のURL"""
 
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
-
 from project.settings.base import MEDIA_ROOT, MEDIA_URL
 from project.urls.base import urlpatterns
 
 urlpatterns += [
-    path("__debug__/", include("debug_toolbar.urls")),
     # Swagger
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
