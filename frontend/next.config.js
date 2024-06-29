dafe0a1b98203c3c106f09f2846d66b0c1493c24
shun198 <shunhiroseluvmri@gmail.com>
2024-06-29 22:12:12 +0900

    👍 update basic auth

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,8 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
-  trailingSlash: true,
+  output: 'standalone',
 };
 
 module.exports = withBundleAnalyzer({ ...nextConfig });
