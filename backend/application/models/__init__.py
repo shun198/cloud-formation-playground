828a35db125d2abece0aca9daf0dc4b983334491
s.hirose <shunichi_hirose@pj100.biz>
2024-08-20 17:04:00 +0900

    👍 delete

--- backend/application/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from application.models.customer import Address, Customer, Photo
-from application.models.product import Product
-from application.models.user import User, UserInvitation, UserResetPassword
--- frontend/pages/password-reset/index.tsx
+++
@@ -1,5 +0,0 @@
-function PasswordReset() {
-  return <h1>パスワードを忘れた</h1>;
-}
-
-export default PasswordReset;
